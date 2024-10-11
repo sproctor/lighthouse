0d9233aa1f91bf3f22b4218072ff5d3fb6d5bda1
Sean Proctor <sproctor@gmail.com>
2024-10-11 14:44:36 -0400

    update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,5 +16,20 @@ dependencyResolutionManagement {
 
 rootProject.name = "lighthouse"
 
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.60.5"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+refreshVersions {
+    // work-around https://github.com/Splitties/refreshVersions/issues/640
+    file("build/tmp/refreshVersions").mkdirs()
+    versionsPropertiesFile = file("build/tmp/refreshVersions/versions.properties")
+    rejectVersionIf {
+        candidate.stabilityLevel.isLessStableThan(current.stabilityLevel)
+    }
+}
+
 include(":lighthouse")
 include(":demo")
